9ba9d0237a2edb8723b694e068fe9b1ab9ac3c98
Tristan Espinoza <tristan.m.espinoza@gmail.com>
2017-05-30 21:13:34 -0700

    Edited shell script to accommodate unassigned example.

--- download-imagenet.sh
+++ download-imagenet.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="example-3D-assigned-images/bin/data/"
+DIR2="example-3D-unassigned-images/bin/data/"
 
 DB_URL="https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.sqlite3"
 WORDS_URL="https://raw.githubusercontent.com/liuliu/ccv/unstable/samples/image-net-2012.words"
@@ -9,9 +10,13 @@ DB_FILE="image-net-2012.sqlite3"
 WORDS_FILE="image-net-2012.words"
 
 mkdir -p $DIR
+mkdir -p $DIR2
+
 if [ ! -e $DB_FILE ] ; then
 echo "Downloading image-net-2012 network (ccv)"
 curl -o $DIR$DB_FILE $DB_URL
 curl -o $DIR$WORDS_FILE $WORDS_URL
+cp $DIR$DB_FILE $DIR2$DB_FILE
+cp $DIR$DWORDS_FILE $DIR2$WORDS_FILE
 fi
 echo "Downloaded: image-net-2012 network (ccv)"
